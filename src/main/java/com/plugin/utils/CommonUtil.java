2792a27731d853d7a49d193fb082737829fbae57
xjxliuxing@163.com <xjxliuxing@163.com>
2023-10-10 03:42:33 +0800

    update fills

--- src/main/java/com/plugin/utils/CommonUtil.java
+++
@@ -1,7 +0,0 @@
-package com.plugin.utils;
-
-import sun.nio.cs.UTF_8;
-
-public class CommonUtil {
-    public static final String UTF_8 = "UTF-8";
-}
--- src/main/java/com/plugin/utils/ConvertUtil.java
+++
@@ -1,9 +0,0 @@
-package com.plugin.utils;
-
-public class ConvertUtil {
-
-    public static int convertVersion(String version) {
-        return Integer.parseInt(version);
-    }
-
-}
