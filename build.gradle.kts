cf8731a8ee19f987cf19253d07ceec91c8b7586a
xjxliuxing@163.com <xjxliuxing@163.com>
2023-10-10 10:41:21 +0800

    update fills

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,10 @@ plugins {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle-api:7.4.2")
-    implementation(gradleApi()) // gradle sdk
+    api("com.android.tools.build:gradle-api:7.4.2")
+    api(gradleApi()) // gradle sdk
 
-    implementation("org.json:json:20230227")// json 依赖库
-    implementation("org.jsoup:jsoup:1.16.1") // html依赖库
+    api("org.json:json:20230227")// json 依赖库
+    api("org.jsoup:jsoup:1.16.1") // html依赖库
 }
 
