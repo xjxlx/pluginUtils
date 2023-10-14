d49428640929783e243a3271b9e0351f83cc8270
xjxliuxing@163.com <xjxliuxing@163.com>
2023-10-15 01:54:36 +0800

    update files

--- Config.kt
+++ Config.kt
@@ -3,4 +3,7 @@ package utils
 object Config {
     const val PROJECT_GROUP = "com.github.jitpack"
     const val PROJECT_VERSION = "1.0"
+
+    const val PUBLISH_PLUGIN_ID = "maven-publish"
+    const val PUBLISH_TYPE = "release"
 }
