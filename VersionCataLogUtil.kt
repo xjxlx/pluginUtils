dc2dd7621c9f290288458bf423d9a203566b1826
xjxliuxing@163.com <xjxliuxing@163.com>
2023-10-21 23:10:54 +0800

    update files

---
+++ VersionCataLogUtil.kt
@@ -0,0 +1,78 @@
+package utils
+
+import common.Config
+import org.gradle.api.Project
+import java.io.FileOutputStream
+
+class VersionCataLogUtil {
+
+    private var mSettingList = mutableListOf<String>()
+
+    private val dependencyResolutionManagement = "dependencyResolutionManagement"
+    private var dependencyResolutionManagementFlag = false
+
+    private val repositoriesMode = "repositoriesMode"
+    private val repositories = "repositories"
+    private var repositoriesFlag = false
+    private var catalogFlag = false
+
+    fun write(project: Project) {
+        val settingsFile =
+            project.rootDir.listFiles()?.find { it.isFile && it.name.contains("settings") }
+
+        settingsFile?.let {
+            FileUtil.readFile(settingsFile)?.let { settingsList ->
+
+                settingsList.forEach { item ->
+                    val trim = item.trim()
+                    // println("item:$item")
+                    if (repositoriesFlag) {
+                        if (trim == "}") {
+                            repositoriesFlag = false
+
+                            // 1:添加中央控制仓库
+                            // mSettingList.add(Config.VersionCatalog.MAVEN_PUBLIC)
+
+                            // 2：添加阿里云：用户信息 - release
+                            // mSettingList.add(Config.VersionCatalog.MAVEN_RELEASE)
+
+                            // 2：添加阿里云：用户信息 - snapshot
+                            // mSettingList.add(Config.VersionCatalog.MAVEN_SNAPSHOT)
+                            catalogFlag = true
+                        }
+                    }
+                    mSettingList.add(item)
+
+                    // 3:添加catalog
+                    if (catalogFlag) {
+                        mSettingList.add(Config.VersionCatalog.CATALOG)
+                        catalogFlag = false
+                    }
+
+                    if (trim.startsWith(dependencyResolutionManagement)) {
+                        dependencyResolutionManagementFlag = true
+                    }
+
+                    if (dependencyResolutionManagementFlag) {
+                        if (trim.startsWith(repositories) && !trim.startsWith(repositoriesMode)) {
+                            dependencyResolutionManagementFlag = false
+                            repositoriesFlag = true
+                        }
+                    }
+                }
+            }
+
+            // mSettingList.forEach {
+            //     println("item - add: $it")
+            // }
+
+            // write data
+            FileOutputStream(settingsFile).use {
+                mSettingList.forEach { item ->
+                    it.write(item.toByteArray())
+                    it.write("\r\n".toByteArray())
+                }
+            }
+        }
+    }
+}
