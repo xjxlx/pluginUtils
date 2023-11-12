b040b6e10dc07a9a2d27de2905d7d4464c678e58
xjxliuxing@163.com <xjxliuxing@163.com>
2023-11-12 21:21:26 +0800

    add model

--- VersionCataLogUtil.kt
+++ VersionCataLogUtil.kt
@@ -9,7 +9,8 @@ class VersionCataLogUtil {
     private val mListContent = arrayListOf<String>()
 
     companion object {
-        private const val ORIGIN_VERSION = "https://github.com/xjxlx/plugins/blob/master/publish/src/main/java/com/android/publish/version/29/version.json"
+        private const val ORIGIN_VERSION =
+            "https://github.com/xjxlx/plugins/blob/master/publish/src/main/java/com/android/publish/version/version.json"
         private const val TAG_PLUGIN_MANAGEMENT = "pluginManagement"
         private const val TAG_DEPENDENCY_RESOLUTION_MANAGEMENT = "dependencyResolutionManagement"
         private const val TAG_REPOSITORIES_MODE = "repositoriesMode"
